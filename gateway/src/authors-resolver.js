5aa5ad6c164ed5342c0302eb97a7a7cb40b16e3a
Nemanja Milosavljevic <nemanja.milosavljevic@newtron.de>
2021-04-13 12:18:32 +0200

    Fix the problem with non working yaml-style resolvers.
    For some reason the documented property `returnData` breaks the resolver without much feedback and the documentation doesn't mention details regarding this property.

--- gateway/src/authors-resolver.js
+++ gateway/src/authors-resolver.js
@@ -1,9 +1,12 @@
-import { __awaiter } from "tslib";
-export const resolvers = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolvers = void 0;
+const tslib_1 = require("tslib");
+exports.resolvers = {
     Book: {
         author: {
             selectionSet: `{ authorId }`,
-            resolve: (root, args, { Authors }, info) => __awaiter(void 0, void 0, void 0, function* () {
+            resolve: (root, args, { Authors }, info) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
                 return yield Authors.api.author({ id: root.authorId });
             }),
         },
@@ -11,7 +14,7 @@ export const resolvers = {
     Author: {
         books: {
             selectionSet: `{ booksIds }`,
-            resolve: (root, args, { Books }, info) => __awaiter(void 0, void 0, void 0, function* () {
+            resolve: (root, args, { Books }, info) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
                 const books = root.booksIds.map((bookId) => Books.api.book({ id: bookId }));
                 // @ts-ignore
                 return yield Promise.all(books);
@@ -19,4 +22,3 @@ export const resolvers = {
         },
     },
 };
-//# sourceMappingURL=authors-resolver.js.map
