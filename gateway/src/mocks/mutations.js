c628fe891ac2c648c9c0658e676cff30755a55c9
Nemanja Milosavljevic <nemanjamilosavljevic@gmail.com>
2021-04-25 17:22:56 +0200

    Improve the intialization script

--- gateway/src/mocks/mutations.js
+++ gateway/src/mocks/mutations.js
@@ -1,15 +1,15 @@
 module.exports = {
   updateAuthor: (_, { id, authorUpdateInput }, { mockStore }) => {
-    console.log('input', id, authorUpdateInput);
-    const authorRef = mockStore.get('Query', 'ROOT', 'author', id);
+    // Get the current state from the store
+    const currentAuthor = mockStore.get('Query', 'ROOT', 'author', id);
 
-    const ref = {
-      $ref: { key: authorRef.$ref.key, typeName: 'Author' },
+    // Update the store
+    const authorRef = {
+      $ref: { key: currentAuthor.$ref.key, typeName: 'Author' },
     };
-    mockStore.set(ref, 'name', authorUpdateInput.name);
+    mockStore.set(authorRef, 'name', authorUpdateInput.name);
 
-    const updatedAuthorRef = mockStore.get('Query', 'ROOT', 'author', id);
-    console.log('store', updatedAuthorRef, mockStore.get(updatedAuthorRef, 'name'));
-    return updatedAuthorRef;
+    // Get
+    return mockStore.get('Query', 'ROOT', 'author', id);
   },
 };
